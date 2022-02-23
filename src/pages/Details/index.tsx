6bc75a2bc45fa663ad227cc7a032279f0c0e69da
Li-HONGYAO <lihy_online@163.com>
2022-02-23 17:50:58 +0800

    更新ant-mobile(白杨)

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Lee
  * @Date: 2021-11-12 14:06:07
  * @LastEditors: Lee
- * @LastEditTime: 2021-11-13 15:15:26
+ * @LastEditTime: 2022-02-23 17:48:07
  */
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -11,7 +11,7 @@ const Details: React.FC = () => {
     <div className='page'>
       <div className='coming-soon'>Demo Example</div>
       <div className='text-center f20 mt-20'>
-        <Link to='/index'>Go Home</Link>
+        <Link to='/'>Go Home</Link>
       </div>
     </div>
   );
--- src/pages/Integral/index.tsx
+++ src/pages/Integral/index.tsx
@@ -2,18 +2,26 @@
  * @Author: Lee
  * @Date: 2021-09-04 14:21:21
  * @LastEditors: Lee
- * @LastEditTime: 2021-12-18 22:40:14
+ * @LastEditTime: 2022-02-23 17:50:27
  */
 
 import Loading from '@/components/@lgs-react/Loading';
 import React from 'react';
+import { Button, Space } from 'antd-mobile';
 import './index.less';
 const Integral: React.FC = () => {
-  document.title='攒积分';
+  document.title = '攒积分';
   return (
     <div className='tab-page'>
       <div className='coming-soon'>Demo Example</div>
-      <Loading tips='加载中' color='blue'/>
+      <Loading tips='加载中' color='blue' />
+      <div className='flex-h-center px-10 mt-10'>
+        <Space wrap>
+          <Button color='primary'>Primary</Button>
+          <Button color='success'>Success</Button>
+          <Button color='danger'>Danger</Button>
+        </Space>
+      </div>
     </div>
   );
 };
