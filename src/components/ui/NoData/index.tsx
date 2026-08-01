9467172966f7292099bffc8d418f9656ede6cd5a
Leo <lihy_online@163.com>
2026-08-01 10:44:44 +0400

    fix: update NoData component

--- src/components/ui/NoData/index.tsx
+++ src/components/ui/NoData/index.tsx
@@ -1,16 +1,14 @@
 import { memo } from 'react';
 
-import './index.less';
-
 export default memo(function NoData({ tips }: { tips?: string }) {
   return (
-    <div className="lg-no-data">
+    <div className="ui-no-data pt-20 text-center">
       <img
-        className="lg-no-data__img"
-        src={new URL('./images/no-data__4.png', import.meta.url).toString()}
         alt=""
+        className="mx-auto w-[171px]"
+        src={new URL('./images/no-data__4.png', import.meta.url).href}
       />
-      {tips && <p className="lg-no-data__tips">{tips}</p>}
+      {tips ? <p className="mt-[21px] text-sm leading-5 text-[#999]">{tips}</p> : null}
     </div>
   );
 });
