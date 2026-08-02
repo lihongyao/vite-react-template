4f4bb4d74f3e404c51df598419c2dc1147c693a0
Leo <lihy_online@163.com>
2026-08-02 19:25:02 +0400

    feat: add box to integral

--- src/pages/Integral/index.tsx
+++ src/pages/Integral/index.tsx
@@ -1,3 +1,16 @@
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-3 p-3">
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+      <div className="h-32 rounded-xl bg-gray-400"></div>
+    </div>
+  );
 }
